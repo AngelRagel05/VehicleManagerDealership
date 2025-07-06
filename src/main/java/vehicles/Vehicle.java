71e49329d86b9aa8d3c44003b54c1c745501363c
AngelRagel05 <jimenezragelangel@gmail.com>
2025-07-06 22:05:45 +0200

    Agregar archivos de configuración y estructura inicial del proyecto

--- src/main/java/vehicles/Vehicle.java
+++ src/main/java/vehicles/Vehicle.java
@@ -1,4 +1,19 @@
 package vehicles;
 
-public class vehicle {
+public abstract class Vehicle {
+
+    String brand;
+    String model;
+    Double price;
+
+    public Vehicle(String brand, String model, Double price) {
+        this.brand = brand;
+        this.model = model;
+        this.price = price;
+    }
+
+    public Vehicle(){}
+
+    void showDetails() {
+    }
 }
