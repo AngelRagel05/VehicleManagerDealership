d81eb5b1e62f5e695217a7a7e757b3035c289657
AngelRagel05 <jimenezragelangel@gmail.com>
2025-07-08 02:21:50 +0200

    Agregar constructores por defecto en InvalidDiscountException e InvalidPriceException y corregir formato en Motorbike.java

--- src/main/java/exceptions/InvalidDiscountException.java
+++ src/main/java/exceptions/InvalidDiscountException.java
@@ -4,4 +4,8 @@ public class InvalidDiscountException extends RuntimeException {
     public InvalidDiscountException(String message) {
         super(message);
     }
+
+    public InvalidDiscountException() {
+        super("El descuento tiene que oscilar entre 0 y 100.");
+    }
 }
--- src/main/java/exceptions/InvalidPriceException.java
+++ src/main/java/exceptions/InvalidPriceException.java
@@ -4,4 +4,8 @@ public class InvalidPriceException extends RuntimeException {
     public InvalidPriceException(String message) {
         super(message);
     }
+
+    public InvalidPriceException() {
+        super("El precio no puede ser menor a 0.");
+    }
 }
